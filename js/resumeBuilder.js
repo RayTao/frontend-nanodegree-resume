5c73f49cc043876eb743190c68476fe3062d04ce
卡普 <kapu@juanpi.com>
2017-05-11 23:11:57 +0800

    akk

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var name = "ray";
+var awesomeThoughts = "I am ray and I am AWESOME!";
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+$("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
